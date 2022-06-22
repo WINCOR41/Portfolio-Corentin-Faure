fa957c3700f4dad5ee1c334a5ca8f3f47079c3fb
WINCOR41 <108020962+WINCOR41@users.noreply.github.com>
2022-06-22 19:30:17 +0200

    Add files via upload

---
+++ assets/js/cookies.js
@@ -0,0 +1,2 @@
+
+<script src="http://www.example.com/cookiechoices.js"></script><script>document.addEventListener('DOMContentLoaded', function(event){cookieChoices.showCookieConsentBar('Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.', 'J’accepte', 'En savoir plus', 'http://www.example.com/mentions-legales/');});</script>
